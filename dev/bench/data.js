14b5cb1ed05b20520ee11eadc7e7318189b227da
github-action-benchmark <github@users.noreply.github.com>
2022-12-28 11:37:55 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for cafca5c5065981281b7d08539bd7a3465e4f1cf6

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1672227475442,
+  "repoUrl": "https://github.com/jchavarri/dune",
+  "entries": {
+    "Melange Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "committer": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "distinct": true,
+          "id": "cafca5c5065981281b7d08539bd7a3465e4f1cf6",
+          "message": "melange: fix some steps in bench workflow\n\nSigned-off-by: Javier Chávarri <javier.chavarri@gmail.com>",
+          "timestamp": "2022-12-28T11:16:35Z",
+          "tree_id": "82c2118f162d804ce0b9e2e2aa2ac940ba21655b",
+          "url": "https://github.com/jchavarri/dune/commit/cafca5c5065981281b7d08539bd7a3465e4f1cf6"
+        },
+        "date": 1672227474273,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "41392.97405990667",
+            "unit": "millisecond"
+          }
+        ]
+      }
+    ]
+  }
+}
