8b712eba9c23b7dd6134c55a85227bd49bb6449d
github-action-benchmark <github@users.noreply.github.com>
2023-03-01 21:21:10 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for b8fd9dffecfd1a102720ca2ab485e80fcee2cb65

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677624136712,
+  "lastUpdate": 1677705670606,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -956,6 +956,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "36.78798283337334",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b8fd9dffecfd1a102720ca2ab485e80fcee2cb65",
+          "message": "fix: custom log file path (#7200)\n\nCreate directory when using a custom path for the log file\r\n\r\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-03-01T11:24:30-08:00",
+          "tree_id": "4713ed049c7d250ddd6841136f703fb08bcadd45",
+          "url": "https://github.com/jchavarri/dune/commit/b8fd9dffecfd1a102720ca2ab485e80fcee2cb65"
+        },
+        "date": 1677705669994,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "35.62957278732",
             "unit": "seconds"
           }
         ]
