7dd743292120eae71a33360971a5bfa2f5abc577
github-action-benchmark <github@users.noreply.github.com>
2023-03-03 12:40:06 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677846829818,
+  "lastUpdate": 1677847206569,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -1712,6 +1712,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (Linux)",
             "value": "68.51026195952667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47",
+          "message": "feature: add terminal ui backend based on NoTTY (#6996)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-03-02T13:48:18-08:00",
+          "tree_id": "fed526d9dc61ac26685496aeb92a7b07d214d084",
+          "url": "https://github.com/jchavarri/dune/commit/8d88ee8068abb053fe8ed7c9c21b3a1883dbaf47"
+        },
+        "date": 1677847205810,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "74.48023360092667",
             "unit": "seconds"
           }
         ]
