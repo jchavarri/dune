451ec93ea6a6b6e816a0e137f11561e9b0347cef
github-action-benchmark <github@users.noreply.github.com>
2023-03-10 20:22:10 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for eec2b08d68cfe8408e662b8e5e897d2c0b8d366d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678445731809,
+  "lastUpdate": 1678479730507,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -1681,6 +1681,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "33.32459674464667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alizter@gmail.com",
+            "name": "Ali Caglayan",
+            "username": "Alizter"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eec2b08d68cfe8408e662b8e5e897d2c0b8d366d",
+          "message": "fix(coq): fix duplicate dir targets being detected before theories (#7263)\n\nSigned-off-by: Ali Caglayan <alizter@gmail.com>",
+          "timestamp": "2023-03-10T09:40:51-08:00",
+          "tree_id": "286b637bce1c9d866d72a50b6822baa53ea1b01c",
+          "url": "https://github.com/jchavarri/dune/commit/eec2b08d68cfe8408e662b8e5e897d2c0b8d366d"
+        },
+        "date": 1678479729790,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "33.19980502966667",
             "unit": "seconds"
           }
         ]
