df2db0a8dfa54a2c679c01faf3581c1ae08e9e7f
github-action-benchmark <github@users.noreply.github.com>
2023-03-10 10:55:31 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for d5582c7d677b0d8afcc92f5c390d014cdac1a97f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678445721287,
+  "lastUpdate": 1678445731809,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -3017,6 +3017,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "64.51665728977999",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anmonteiro@gmail.com",
+            "name": "Antonio Nuno Monteiro",
+            "username": "anmonteiro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d5582c7d677b0d8afcc92f5c390d014cdac1a97f",
+          "message": "chore(nix): remove the opam2nix dependency on the default package (#7258)\n\nSigned-off-by: Antonio Nuno Monteiro <anmonteiro@gmail.com>",
+          "timestamp": "2023-03-09T15:09:35-08:00",
+          "tree_id": "0e579760f62fdb226997e861522d4c545a9d5816",
+          "url": "https://github.com/jchavarri/dune/commit/d5582c7d677b0d8afcc92f5c390d014cdac1a97f"
+        },
+        "date": 1678445730717,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.5644264263133332",
             "unit": "seconds"
           }
         ]
