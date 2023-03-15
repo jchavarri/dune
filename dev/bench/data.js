da5505139546b770e0199379855be9af086c9b22
github-action-benchmark <github@users.noreply.github.com>
2023-03-15 15:49:05 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 3e6260d682799fb6a240200034b71f3d58194bc2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1678889834562,
+  "lastUpdate": 1678895345389,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -3686,6 +3686,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "31.290300241826667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "committer": {
+            "email": "javier.chavarri@gmail.com",
+            "name": "Javier Chávarri",
+            "username": "jchavarri"
+          },
+          "distinct": true,
+          "id": "3e6260d682799fb6a240200034b71f3d58194bc2",
+          "message": "Merge branch 'main' into benchmark/add-synthetic-watch\nSigned-off-by: Javier Chávarri <javier.chavarri@gmail.com>",
+          "timestamp": "2023-03-15T13:47:09Z",
+          "tree_id": "5c85ce2656211fb0eaf60e5aa63e2a17f2dcad69",
+          "url": "https://github.com/jchavarri/dune/commit/3e6260d682799fb6a240200034b71f3d58194bc2"
+        },
+        "date": 1678895344743,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "32.56514388545333",
             "unit": "seconds"
           }
         ]
