d426e4e75cdc9d346f1f8796699da0392cc13f3f
github-action-benchmark <github@users.noreply.github.com>
2023-03-18 19:41:54 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 4245029f2914bcfdca915e9916a5155a0afa95fb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679168503833,
+  "lastUpdate": 1679168514343,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -3800,6 +3800,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "65.19511540498667",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4245029f2914bcfdca915e9916a5155a0afa95fb",
+          "message": "feature: add action runners (#6737)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-03-17T16:47:22-07:00",
+          "tree_id": "c854afadcc9812714d9415eca4c789aeb33a416e",
+          "url": "https://github.com/jchavarri/dune/commit/4245029f2914bcfdca915e9916a5155a0afa95fb"
+        },
+        "date": 1679168513362,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "1.5724990190066668",
             "unit": "seconds"
           }
         ]
