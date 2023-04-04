eece87455d3e554e0de4fef41a6b335fe1167f9c
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 09:23:10 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 6282035fbe9ff242381e6979b7a1645977c45431

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680600176461,
+  "lastUpdate": 1680600190641,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -4409,6 +4409,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "89.01756897198",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6282035fbe9ff242381e6979b7a1645977c45431",
+          "message": "refactor: stop qualifying Lang.Instance.t (#7482)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-04-03T17:33:11-07:00",
+          "tree_id": "cc15f78f499fc34bba3607c0429b9515822d3a11",
+          "url": "https://github.com/jchavarri/dune/commit/6282035fbe9ff242381e6979b7a1645977c45431"
+        },
+        "date": 1680600189829,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.3484103419666664",
             "unit": "seconds"
           }
         ]
