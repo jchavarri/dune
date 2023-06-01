2086621e9378b9deccd74a8f2aa330a0b08bbadc
github-action-benchmark <github@users.noreply.github.com>
2023-06-01 07:57:12 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for d4ceaa91ff88c59a16b9437046e9379e8fa38d34

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1685605793857,
+  "lastUpdate": 1685606232153,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -4989,6 +4989,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.6716013454999998",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sbaugh@catern.com",
+            "name": "Spencer Baugh",
+            "username": "catern"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d4ceaa91ff88c59a16b9437046e9379e8fa38d34",
+          "message": "refactor: create Diagnostic.to_user_message so it can be reused (#7812)\n\nAnother patch wanted to use this function, so move it out of private\r\nbin/diagnostics.ml to somewhere it can be reused.\r\n\r\nSigned-off-by: Spencer Baugh <sbaugh@janestreet.com>\r\nCo-authored-by: Spencer Baugh <sbaugh@janestreet.com>",
+          "timestamp": "2023-05-31T08:02:27-07:00",
+          "tree_id": "0271672647bc47b88629f595080a2aac6e7d3cff",
+          "url": "https://github.com/jchavarri/dune/commit/d4ceaa91ff88c59a16b9437046e9379e8fa38d34"
+        },
+        "date": 1685606231048,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "86.74953235068666",
             "unit": "seconds"
           }
         ]
