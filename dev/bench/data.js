9027012bfb14ba94d8cb1d84a4aca4220cef24d3
github-action-benchmark <github@users.noreply.github.com>
2023-06-07 18:53:55 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for bd1193de7b5b59770d01d0a3f43e6936c6271a16

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686164022094,
+  "lastUpdate": 1686164035444,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -5105,6 +5105,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (cold, Linux)",
             "value": "78.70655478297333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@emillon.org",
+            "name": "Etienne Millon",
+            "username": "emillon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bd1193de7b5b59770d01d0a3f43e6936c6271a16",
+          "message": "doc: .opam.template files can be generated (#7911)\n\nSigned-off-by: Etienne Millon <me@emillon.org>",
+          "timestamp": "2023-06-07T14:04:52+02:00",
+          "tree_id": "812657e8e73fb243601ac298fdfa3b34dcd19293",
+          "url": "https://github.com/jchavarri/dune/commit/bd1193de7b5b59770d01d0a3f43e6936c6271a16"
+        },
+        "date": 1686164034462,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (warm, Linux)",
+            "value": "2.1524167999666672",
             "unit": "seconds"
           }
         ]
