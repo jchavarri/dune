f837e314193d90b74a35a46f67d781a402c2ac98
github-action-benchmark <github@users.noreply.github.com>
2023-06-30 14:25:26 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 3686afa0b26df29fcb9f4f199ca5c4ff39b544f9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1688134695969,
+  "lastUpdate": 1688135126163,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -2406,6 +2406,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "46.4045747395",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3686afa0b26df29fcb9f4f199ca5c4ff39b544f9",
+          "message": "refactor: remove some unused code (#8084)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-06-30T05:49:14-07:00",
+          "tree_id": "5413f437f1ad368a606f834a12d7be654dc95be4",
+          "url": "https://github.com/jchavarri/dune/commit/3686afa0b26df29fcb9f4f199ca5c4ff39b544f9"
+        },
+        "date": 1688135125153,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "41.11615140181333",
             "unit": "seconds"
           }
         ]
