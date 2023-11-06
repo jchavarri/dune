e9d9068c34e6b80864aa27772ea618451db7b39d
github-action-benchmark <github@users.noreply.github.com>
2023-11-06 07:40:21 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 9cd2757adf94f681ffcf83650576794e089c14c3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698132190377,
+  "lastUpdate": 1699256421593,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -6876,6 +6876,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "16.076988607513332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9cd2757adf94f681ffcf83650576794e089c14c3",
+          "message": "refactor(pkg): split pkg commands into modules (#9093)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2023-11-06T06:49:05Z",
+          "tree_id": "95b9f92cc4bca06bbfc8aff6ddf1440f7eea9748",
+          "url": "https://github.com/jchavarri/dune/commit/9cd2757adf94f681ffcf83650576794e089c14c3"
+        },
+        "date": 1699256420454,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "21.63820437118667",
             "unit": "seconds"
           }
         ]
