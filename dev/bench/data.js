27a16f44b0175195a9126fe7e5b0e3da300cb2ab
github-action-benchmark <github@users.noreply.github.com>
2024-01-24 10:12:01 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for a379928b3cced11e50318d3cdb30d630ae97eede

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706090851271,
+  "lastUpdate": 1706091121009,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -2783,6 +2783,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "23.23210663302",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a379928b3cced11e50318d3cdb30d630ae97eede",
+          "message": "refactor: move [status_map] to own module (#9825)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2024-01-24T09:43:01Z",
+          "tree_id": "76ca3762acdb86c4e4ae7757a66162b51ea9eea3",
+          "url": "https://github.com/jchavarri/dune/commit/a379928b3cced11e50318d3cdb30d630ae97eede"
+        },
+        "date": 1706091120435,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "23.080312068426664",
             "unit": "seconds"
           }
         ]
