cfec053b244d25d6d840937a7a3f872f4c75f84f
github-action-benchmark <github@users.noreply.github.com>
2024-02-02 10:48:52 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for 21557d8d0b68650d4997c5ec94a1a782a2847a87

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706870660762,
+  "lastUpdate": 1706870932501,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -2986,6 +2986,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "22.977753969233333",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marek@tarides.com",
+            "name": "Marek Kubica",
+            "username": "Leonidas-from-XIV"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "21557d8d0b68650d4997c5ec94a1a782a2847a87",
+          "message": "feature(pkg): Submodule support (#9798)\n\n* feature(pkg): Submodule support\r\n\r\nSigned-off-by: Marek Kubica <marek@tarides.com>",
+          "timestamp": "2024-02-02T10:09:44Z",
+          "tree_id": "440c7e860fd2d0cffd7a25f18372c6e2224db5b2",
+          "url": "https://github.com/jchavarri/dune/commit/21557d8d0b68650d4997c5ec94a1a782a2847a87"
+        },
+        "date": 1706870931821,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "22.867193062613335",
             "unit": "seconds"
           }
         ]
