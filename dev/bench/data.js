4d8f71c94b7238a96d961457fd50994b3017f09a
github-action-benchmark <github@users.noreply.github.com>
2024-02-14 16:49:45 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for f932c9f106268c401a02eeaae6f89a6b83b2aad5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707841497476,
+  "lastUpdate": 1707929385781,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -9080,6 +9080,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "13.691055709653332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f932c9f106268c401a02eeaae6f89a6b83b2aad5",
+          "message": "refactor: remove useless field from [Readdir] (#10024)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2024-02-14T05:16:50Z",
+          "tree_id": "071a9d9d3662ddcbc0a77bd2737f8447aed38c26",
+          "url": "https://github.com/jchavarri/dune/commit/f932c9f106268c401a02eeaae6f89a6b83b2aad5"
+        },
+        "date": 1707929384371,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "14.275076371766668",
             "unit": "seconds"
           }
         ]
