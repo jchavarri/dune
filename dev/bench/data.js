42cce412da160bf14e8146801347386655261bff
github-action-benchmark <github@users.noreply.github.com>
2024-03-08 11:47:03 +0000

    add Synthetic Benchmark (customSmallerIsBetter) benchmark result for 89f1c299ad1710686c972f057a55c7da47a3d674

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709898206722,
+  "lastUpdate": 1709898423039,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -8469,6 +8469,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic build time (warm, Linux)",
             "value": "1.2049832679133332",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "89f1c299ad1710686c972f057a55c7da47a3d674",
+          "message": "refactor: remove some more one letter vars (#10237)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2024-03-08T11:15:54Z",
+          "tree_id": "f8d8a7e5ddc00f7d62e1a529139a2a521db857a3",
+          "url": "https://github.com/jchavarri/dune/commit/89f1c299ad1710686c972f057a55c7da47a3d674"
+        },
+        "date": 1709898421990,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic build time (cold, Linux)",
+            "value": "42.410328658093334",
             "unit": "seconds"
           }
         ]
