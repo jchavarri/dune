01536dfe2c2c4b5e4b70a8d16f7654b15785348a
github-action-benchmark <github@users.noreply.github.com>
2024-03-13 08:03:05 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 5980dfda00eea658e5680596a53378cecf16fcb7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710165078785,
+  "lastUpdate": 1710316985772,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -11023,6 +11023,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "13.669416719406668",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "stephen@sherra.tt",
+            "name": "Stephen Sherratt",
+            "username": "gridbugs"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5980dfda00eea658e5680596a53378cecf16fcb7",
+          "message": "Add test reproducing gh10254 (#10255)\n\nSigned-off-by: Stephen Sherratt <stephen@sherra.tt>",
+          "timestamp": "2024-03-13T01:54:10Z",
+          "tree_id": "af16842ae18c9c3a6bec1ad0f2ce3ca780b87fc4",
+          "url": "https://github.com/jchavarri/dune/commit/5980dfda00eea658e5680596a53378cecf16fcb7"
+        },
+        "date": 1710316984762,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "13.825171021906668",
             "unit": "seconds"
           }
         ]
