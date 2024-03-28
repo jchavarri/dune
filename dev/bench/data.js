01aed38e68bbe25e51b717e2beefc0ff141d28bb
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 14:10:49 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for a5e6f97f2399cefd18ff38449d61030fc9f651ec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711634890926,
+  "lastUpdate": 1711635049778,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -3798,6 +3798,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "21.81836005208",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "me@rgrinberg.com",
+            "name": "Rudi Grinberg",
+            "username": "rgrinberg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a5e6f97f2399cefd18ff38449d61030fc9f651ec",
+          "message": "refactor: remove pointless conversion in [Command.t] (#10278)\n\nSigned-off-by: Rudi Grinberg <me@rgrinberg.com>",
+          "timestamp": "2024-03-28T11:28:14Z",
+          "tree_id": "92fa3598aa8df780060f3017682b7089ad0b49a1",
+          "url": "https://github.com/jchavarri/dune/commit/a5e6f97f2399cefd18ff38449d61030fc9f651ec"
+        },
+        "date": 1711635048755,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "22.647470030526666",
             "unit": "seconds"
           }
         ]
