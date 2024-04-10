56d9ad410eed887358349bf847bae2baebdcf35e
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 13:14:55 +0000

    add Melange Benchmark (customSmallerIsBetter) benchmark result for ec4081d6596321e1044719564655f620b3372abd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712754744690,
+  "lastUpdate": 1712754895225,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -3914,6 +3914,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "pupilfirst build time (Linux)",
             "value": "22.329038471679997",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "marek@tarides.com",
+            "name": "Marek Kubica",
+            "username": "Leonidas-from-XIV"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ec4081d6596321e1044719564655f620b3372abd",
+          "message": "fix(doc): Correct `versionadded` reference for `subst` (#10406)\n\nSigned-off-by: Marek Kubica <marek@tarides.com>",
+          "timestamp": "2024-04-10T11:54:31+02:00",
+          "tree_id": "d3f4d19c47700035537c31ad4ab78e45cc232121",
+          "url": "https://github.com/jchavarri/dune/commit/ec4081d6596321e1044719564655f620b3372abd"
+        },
+        "date": 1712754894373,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pupilfirst build time (Linux)",
+            "value": "21.87548509238667",
             "unit": "seconds"
           }
         ]
