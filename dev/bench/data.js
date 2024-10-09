0a997e563965cf0dbd8ca6db85c89bb902f19b44
github-action-benchmark <github@users.noreply.github.com>
2024-10-09 14:51:34 +0000

    add Synthetic Watch Benchmark (customSmallerIsBetter) benchmark result for 412a0f5054e6018f724fd6179f8bf53eacc121bf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725801209307,
+  "lastUpdate": 1728485494033,
   "repoUrl": "https://github.com/jchavarri/dune",
   "entries": {
     "Melange Benchmark": [
@@ -14387,6 +14387,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "synthetic watch build time (warm, Linux)",
             "value": "15.660687478239998",
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "moyodiallo@gmail.com",
+            "name": "Alpha Issiaga DIALLO",
+            "username": "moyodiallo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "412a0f5054e6018f724fd6179f8bf53eacc121bf",
+          "message": "test(pkg): Reproduce depexts message bug (#11004)\n\nSigned-off-by: Alpha DIALLO <moyodiallo@gmail.com>",
+          "timestamp": "2024-10-08T20:08:41+01:00",
+          "tree_id": "2e1fab8e5ca03660678ad6dd5615ccb28292dc8c",
+          "url": "https://github.com/jchavarri/dune/commit/412a0f5054e6018f724fd6179f8bf53eacc121bf"
+        },
+        "date": 1728485492332,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "synthetic watch build time (warm, Linux)",
+            "value": "15.983845050059998",
             "unit": "seconds"
           }
         ]
